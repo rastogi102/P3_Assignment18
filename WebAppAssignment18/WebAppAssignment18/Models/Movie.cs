dd9eecd594c1799ee6be8b8695d25ad97e65069a
rastogi102 <nikhilrastogi102@gmail.com>
2023-09-05 15:57:16 +0530

    initial commit

---
+++ WebAppAssignment18/WebAppAssignment18/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Xml.Linq;  
+
+namespace WebAppAssignment18.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
